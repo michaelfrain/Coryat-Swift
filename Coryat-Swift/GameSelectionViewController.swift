a477c9142edc844fb25e065d60f4758c5cb43d16
Michael Frain <michael.frain@hudl.com>
2015-02-02 17:08:56 -0800

    Bootstrapping the app

---
+++ Coryat-Swift/GameSelectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameSelectionViewController.swift
+//  Coryat-Swift
+//
+//  Created by Michael Frain on 2/2/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+import UIKit
+
+class GameSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
