07c2ed8f53012f7ad5f50a97a69b17ef8177fb0e
Michael Frain <michael.frain@hudl.com>
2015-02-07 19:48:09 -0800

    Fix relationship between clues and game

--- Game.swift
+++ Game.swift
@@ -2,7 +2,7 @@
 //  Coryat-Swift.swift
 //  Coryat-Swift
 //
-//  Created by Michael Frain on 2/3/15.
+//  Created by Michael Frain on 2/7/15.
 //  Copyright (c) 2015 Michael Frain. All rights reserved.
 //
 
@@ -11,18 +11,18 @@ import CoreData
 
 class Game: NSManagedObject {
 
-    @NSManaged var gameType: NSNumber
+    @NSManaged var correctResponses: NSNumber
+    @NSManaged var finalResponseCorrect: NSNumber
     @NSManaged var gameDate: NSDate
     @NSManaged var gameIndex: NSNumber
-    @NSManaged var score: NSNumber
-    @NSManaged var correctResponses: NSNumber
+    @NSManaged var gameType: NSNumber
     @NSManaged var incorrectResponses: NSNumber
     @NSManaged var noResponses: NSNumber
-    @NSManaged var finalResponseCorrect: NSNumber
+    @NSManaged var score: NSNumber
     @NSManaged var trashCorrect: NSNumber
     @NSManaged var trashScore: NSNumber
-    @NSManaged var clues: NSSet
-    
+    @NSManaged var clues: NSOrderedSet
+
     class func createGame(context: NSManagedObjectContext) -> Game {
         let newGame = NSEntityDescription.insertNewObjectForEntityForName("Game", inManagedObjectContext: context) as Game
         let oldGames = self.readAllGames(context)
